f6c809a59a17103b8d073b2eee3465be49d3747c
Michaela Warhurst <Michaela.warhurst@knights.ucf.edu>
2018-10-03 11:01:27 -0400

    Added Touch Control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,24 +8,56 @@ public class PlayerController : MonoBehaviour {
 	public float moveSpeed = 10f;
 	public static int meter;
 	public Text meterText;
+	/*public GameObject character;
+	private Rigidbody characterBody; //
+	private float ScreenWidth; //*/
 
 	// Use this for initialization
 	void Start () {
 		meter = 50;
 		meterText.text = meter.ToString();
+//initialization
+		/*ScreenWidth = Screen.width; //
+		characterBody = character.GetComponent<Rigidbody>(); //*/
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, 0f, 0f);
-		meterText.text = meter.ToString();
+	//	transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, 0f, 0f);
+		meterText.text = meter.ToString(); //move player part
 
 		if (Input.GetKeyDown (KeyCode.Escape)) {
-			SceneManager.LoadScene ("MainMenu");
+		SceneManager.LoadScene ("MainMenu");
 		}
 
+		/*int i = 0; //
+
+		while (i < Input.touchCount){
+			if (Input.GetTouch(i).position.x > ScreenWidth/2){
+				//move right
+				RunCharacter(1.0f);//
+			}
+			if (Input.GetTouch(i).position.x < ScreenWidth/2){
+				//moveleft
+				RunCharacter(-1.0f);//
+			}
+			++i;*/
+
+		//}
 	}
+
+	/*	void FixedUpdate(){
+			#if UNITY_EDITOR
+			RunCharacter(Input.GetAxis("Horizontal"));
+			#endif
+		}
+
+		private void RunCharacter(float horizontalInput){
+//move player//
+			characterBody.AddForce(new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0)); //
+
+		}*/
 
 
 	void OnCollisionEnter(Collision other) {
