769bdbaf3c12b597543c30a8395edc7f996d8c95
kmpatterson <33130979+kmpatterson@users.noreply.github.com>
2018-10-05 16:28:16 -0400

    Animated Text and added new scripts
    -Added text animation script (fade-in)
    -Added transition to level script (unfinished)

---
+++ Assets/Scripts/Scene Navigation/SceneTransitionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		//Scene currentScene = SceneManager.GetActiveScene ();
+		//string sceneName = currentScene.name;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( (Input.touchCount >= 1) && (SceneManager.GetActiveScene() == SceneManager.GetSceneByName ("Kayla'sScene-LevelText")) ) {
+			SceneManager.LoadScene ("Prototype Scene");
+		}
+	}
+}
---
+++ Assets/Scripts/TextFadeIn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextFadeIn : MonoBehaviour {
+	private TextMeshPro textMesh;
+	public Color color;
+	public float duration = 1.5f;
+	float alpha;
+
+
+	// Use this for initialization
+	void Start () {
+		textMesh = GetComponent<TextMeshPro> ();
+		textMesh.color = color;
+		alpha = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Color -= new Color (225,225,225, Time.deltaTime);
+		alpha += Time.deltaTime / duration;
+		textMesh.color = new Color (textMesh.color.r, textMesh.color.g, textMesh.color.b, alpha);
+	}
+}
